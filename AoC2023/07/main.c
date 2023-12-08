ec731e6838858884df098d66320cbbe7143a2287
Iceometric <kenny.ahrling@gmail.com>
2023-12-08 09:25:14 +0100

    Day 7 p1 solution first draft

---
+++ AoC2023/07/main.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32
+#define LINE_COUNT 1100
+
+#define HAND_SIZE 5
+#define CARD_TYPE_COUNT 13
+
+enum Status {
+    SUCCESS,
+    INPUT_ERROR,
+    FILE_ERROR
+};
+
+/** Lower the index the stronger the hand */
+typedef enum HandType {
+    FIVE_OF_A_KIND = 0,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    THREE_OF_A_KIND,
+    TWO_PAIR,
+    ONE_PAIR,
+    HIGH_CARD
+} HandType;
+
+typedef struct Card {
+    char hand[HAND_SIZE + 1];
+    HandType type;
+    int bid;
+} Card;
+
+/** Lower the index the stronger the card */
+const char CARDS[] = { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+int get_lines(FILE *file, char buffer[LINE_COUNT][BUFFER_SIZE]) {
+    char c;
+    int i, line;
+    i = line = 0;
+    while (EOF != (c = getc(file))) {
+        buffer[line][i++] = c;
+        if (c == '\n') {
+            buffer[line][i] = '\0';
+            i = 0;
+            ++line;
+        }
+    }
+    return line;
+}
+
+size_t get_card_index(char c) {
+    for (size_t i = 0; i < CARD_TYPE_COUNT; ++i) {
+        if (c == CARDS[i]) return i;
+    }
+    return CARD_TYPE_COUNT;
+}
+
+void set_hand_type(Card *card) {
+    int map[CARD_TYPE_COUNT] = {0};
+    for (size_t i = 0; i < HAND_SIZE; ++i) 
+        map[get_card_index(card->hand[i])]++;
+
+    // printf("%s\n", card->hand);
+    // for (size_t i = 0; i < CARD_TYPE_COUNT; ++i)
+    //     printf("map %zu: %d\n", i, map[i]);
+
+    int count[5] = {0};
+    for (size_t i = 0; i < CARD_TYPE_COUNT; ++i)
+        switch(map[i]) {
+            case 0: break;
+            case 1: count[0]++; break;
+            case 2: count[1]++; break;
+            case 3: count[2]++; break;
+            case 4: count[3]++; break;
+            case 5: count[4]++; break;
+        }
+
+    // for (size_t i = 0; i < HAND_SIZE; ++i)
+    //     printf("%zu's: %d\n", i, count[i]);
+
+    if (count[4] == 1) {
+        card->type = FIVE_OF_A_KIND;
+        return;
+    }
+    if (count[3] == 1) {
+        card->type = FOUR_OF_A_KIND;
+        return;
+    }
+    if (count[2] == 1 && count[1] == 1) {
+        card->type = FULL_HOUSE;
+        return;
+    }
+    if (count[2] == 1) {
+        card->type = THREE_OF_A_KIND;
+        return;
+    }
+    if (count[1] == 2) {
+        card->type = TWO_PAIR;
+        return;
+    }
+    if (count[1] == 1) {
+        card->type = ONE_PAIR;
+        return;
+    }
+
+    card->type = HIGH_CARD;
+}
+
+void get_hand_data(char *c, Card *card) {
+    for (size_t i = 0; i < HAND_SIZE; ++i) {
+        card->hand[i] = c[i];
+        if (i == HAND_SIZE - 1) card->hand[++i] = '\0';
+    }
+    card->bid = atoi(&c[HAND_SIZE + 1]);
+    set_hand_type(card);
+}
+
+void print_card_data(Card *card) {
+    printf("%s, bid: %d type: %d\n", card->hand, card->bid, card->type);
+}
+
+int lowest_hand_first(Card *c1, Card *c2) {
+    int value;
+    for (size_t i = 0; i < HAND_SIZE; ++i) {
+        value = get_card_index(c2->hand[i]) - get_card_index(c1->hand[i]);
+        if (value != 0) break;
+    }
+    return value;
+}
+
+int lowest_value_first(const void *a, const void *b) {
+    Card *c1 = (Card*)a;
+    Card *c2 = (Card*)b;
+
+    int value = c2->type - c1->type;
+    if (value == 0) value = lowest_hand_first(c1, c2);
+
+    return value;
+}
+
+int get_hands_sum(FILE *file) {
+    char buffer[LINE_COUNT][BUFFER_SIZE];
+    int count = get_lines(file, buffer);
+
+    for (size_t i = 0; i < count; ++i)
+        printf("%s", &buffer[i][0]);
+
+    Card cards[count];
+
+    for (size_t i = 0; i < count; ++i) printf("%s", &buffer[i][0]);
+
+    // populate struct
+    for (size_t i = 0; i < count; ++i)
+        get_hand_data(&buffer[i][0], &cards[i]);
+
+    // for (size_t i = 0; i < count; ++i)
+    //     print_card_data(&cards[i]);
+
+    // sort array
+    qsort(&cards, count, sizeof(Card), lowest_value_first);
+
+    // printf("sorted\n");
+    // for (size_t i = 0; i < count; ++i)
+    //     print_card_data(&cards[i]);
+    
+    int sum = 0;
+    // multiply score
+    for (size_t i = 0; i < count; ++i)
+        sum += cards[i].bid * (i + 1);
+
+    return sum;
+}
+
+void run(void) {
+    FILE *input = fopen("input.txt", "r");
+    if (!input) exit(-FILE_ERROR);
+    printf("The hands sum is: %d\n", get_hands_sum(input));
+    fclose(input);
+}
+
+int main(void) {
+    run();
+    return SUCCESS;
+}
